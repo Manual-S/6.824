8304b5f61f5d1569c76236b0eb85825dfe93186c
pedroshang <pedroshang@tencent.com>
2022-08-25 20:59:40 +0800

    ADD:超时时间设置

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -18,6 +18,8 @@ package raft
 //
 
 import (
+	"log"
+	"math/rand"
 	"sync"
 	"time"
 )
@@ -44,6 +46,12 @@ type ApplyMsg struct {
 	CommandIndex int
 }
 
+const (
+	Leader    = 1
+	Follower  = 2
+	Candidate = 3
+)
+
 //
 // A Go object implementing a single Raft peer.
 //
@@ -57,9 +65,16 @@ type Raft struct {
 	// Your data here (2A, 2B, 2C).
 	// Look at the paper's Figure 2 for a description of what
 	// state a Raft server must maintain.
-	currentTerm int       // 服务器已知最新的任期
-	votedFor    *int      // 当前任期内收到选票的候选人 如果没有投给任何候选人 则为空
-	TimeOut     time.Time // 超时时间
+	currentTerm int  // 服务器当前的任期
+	votedFor    *int // 当前任期内收到选票的候选人 如果没有投给任何候选人 则为空
+	//TimeOut     time.Time // 超时时间
+	State int // 当前节点的状态
+
+	electionChan  chan bool
+	heartbeatChan chan bool
+
+	electionTimeout  int64 // 超时时间
+	electionDuration int64
 }
 
 // return currentTerm and whether this server
@@ -123,8 +138,8 @@ type AppendEntriesReply struct {
 //
 type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
-	CandidateTerm  int // 要求投票的任期
-	CandidateIndex int // 候选人的id
+	CandidateTerm int // 要求投票的任期
+	CandidateId   int // 候选人的id
 }
 
 //
@@ -133,8 +148,9 @@ type RequestVoteArgs struct {
 //
 type RequestVoteReply struct {
 	// Your data here (2A).
-	votedFor *int // 将票投给谁
-
+	votedFor  *int // 将票投给谁
+	votedTerm int  // 投票人的任期
+	votedID   int  // 投票人的id
 }
 
 //
@@ -150,12 +166,12 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	}
 
 	if rf.votedFor != nil {
-		rf.votedFor = &args.CandidateIndex
+		rf.votedFor = &args.CandidateId
 		reply.votedFor = &rf.me
 	}
 }
 
-// 发送请求投票信息到follower的rpc
+// 发送请求投票信息
 // example code to send a RequestVote RPC to a server.
 // server is the index of the target server in rf.peers[].
 // expects RPC arguments in args.
@@ -234,6 +250,84 @@ func (rf *Raft) killed() bool {
 	return z == 1
 }
 
+func (rf *Raft) mainLoop() {
+	for {
+		select {
+		case <-rf.electionChan:
+			// 开启投票流程
+			rf.startElection()
+		case <-rf.heartbeatChan:
+			// 广播心跳包
+		}
+	}
+}
+
+func (rf *Raft) startElection() {
+	rf.State = Candidate
+	rf.currentTerm = rf.currentTerm + 1
+
+	votes := 1 // 自己投自己一票
+
+	reply := RequestVoteReply{}
+
+	for i := 0; i < len(rf.peers); i++ {
+		if i == rf.me {
+			continue
+		}
+
+		go func(id int) {
+			// 发送广播
+			args := RequestVoteArgs{}
+			args.CandidateTerm = rf.currentTerm
+			args.CandidateId = rf.me
+			if rf.sendRequestVote(id, &args, &reply) {
+
+				if rf.currentTerm < reply.votedTerm {
+					log.Printf("currentTerm is %v,votedTerm is %v,Failed vote", rf.currentTerm, reply.votedTerm)
+					rf.State = Follower
+					rf.currentTerm = reply.votedTerm
+					return
+				}
+
+				if reply.votedFor != nil && *(reply.votedFor) == rf.me {
+					votes++
+
+					if votes > len(rf.peers)/2 && rf.State == Candidate {
+						// 成为leader
+						rf.State = Leader
+						// 发送广播包 leader当选
+					}
+				} else {
+					log.Printf("votedId %v reject vote,", reply.votedID)
+				}
+			} else {
+				// 发送广播失败
+				log.Printf("sendRequestVote error,id is %v", id)
+			}
+
+		}(i)
+	}
+
+}
+
+func (rf *Raft) resetTimerElection() {
+	rand.Seed(time.Now().Unix())
+	// todo 这里对时间的处理可能有问题
+	rf.electionDuration = rand.Int63n(150)
+	rf.electionTimeout = time.Now().UnixNano() + rf.electionDuration
+}
+
+// 超时后向chan中发送消息
+func (rf *Raft) timerElection() {
+	for {
+		if time.Now().UnixNano()-rf.electionTimeout > 0 {
+			log.Printf("%d timeout, current term %v,current status %v", rf.me, rf.currentTerm, rf.State)
+			rf.electionChan <- true
+		}
+		time.Sleep(time.Millisecond * 10)
+	}
+}
+
 //
 // the service or tester wants to create a Raft server. the ports
 // of all the Raft servers (including this one) are in peers[]. this
@@ -256,6 +350,9 @@ func Make(peers []*labrpc.ClientEnd, me int,
 
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
+
+	go rf.mainLoop()
+	go rf.timerElection()
 
 	return rf
 }
